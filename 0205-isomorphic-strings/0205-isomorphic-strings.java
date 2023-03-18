8edaa461a9b26dbb585774defd0639ee73a8c49a
sonjix <101625609+onjix@users.noreply.github.com>
2023-03-18 13:15:50 +0900

    Time: 6 ms (84.44%), Space: 42.7 MB (24.48%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] ar1 = new int[200];
+        int[] ar2 = new int[200];
+        
+        if(s.length() != t.length()) {
+            return false;
+        }
+        
+        for(int i=0;i<s.length();i++) {
+            if(ar1[s.charAt(i)] != ar2[t.charAt(i)]) {
+                return false;
+            }
+            
+            ar1[s.charAt(i)] = i+1;
+            ar2[t.charAt(i)] = i+1;
+        }
+        
+        return true;
+    }
+}
